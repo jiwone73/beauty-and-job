5aa66c6fdf60ed3ed0120a58ad7992c04c7286a7
HAJIWON <jiwone73@naver.com>
2026-06-07 23:19:39 +0900

    feat: 모바일 검색 영역에 '어떤 일자리를 찾으세요?' 라벨 추가

--- components/HeroMobile.tsx
+++ components/HeroMobile.tsx
@@ -55,6 +55,7 @@ export default function HeroMobile() {
 
       {/* 검색 영역 (2단) */}
       <form className="hero-m-search-wrap" onSubmit={handleSearch}>
+        <p className="hero-m-search-label">어떤 일자리를 찾으세요?</p>
         {/* 윗줄: 시도 + 구군 */}
         <div className="hero-m-region-row">
           <select
