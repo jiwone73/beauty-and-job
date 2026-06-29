e3733128fa4b68c76ed5d3ae22181fa3345ab308
HAJIWON <jiwone73@naver.com>
2026-06-29 23:36:35 +0900

    전체 noindex 헤더 추가 (개발 중 검색 차단) + vercel.app 주소 beauty-work 전환

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow, noarchive, nosnippet",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 module.exports = nextConfig;
